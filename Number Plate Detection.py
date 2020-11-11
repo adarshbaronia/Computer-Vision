0f25a508547d31c9dbd7fceb52ab5f32ec82ed30
Adarsh Raj Baronia <37274176+adarshbaronia@users.noreply.github.com>
2020-11-11 13:44:03 +0530

    Update Number Plate Detection.py

--- Number Plate Detection.py
+++ Number Plate Detection.py
@@ -32,7 +32,7 @@ while True:
     
     cv2.imshow("Video",img)
     if cv2.waitKey(1) & 0xFF==ord('s'):
-        cv2.imwrite('/Users/dharmendraverma/Downloads/Python/New projects/Object Detection/Numberplate/NumberPlate_'+str(count)+".jpg", imgROI)
+        cv2.imwrite('Numberplate/NumberPlate_'+str(count)+".jpg", imgROI)
         cv2.rectangle(img,(0,200),(640,300),(0,255,0),cv2.FILLED)
         cv2.putText(img,"Scanned Saved"+str(count),(150,265),cv2.FONT_HERSHEY_DUPLEX,2,(0,0,255),2)
         cv2.imshow("Result",img)
